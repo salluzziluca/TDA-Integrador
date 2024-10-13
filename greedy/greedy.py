5599e7530fc27ca9810548eb22d13cb00c521751
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-10-13 17:52:21 -0300

    fix magic numbers

--- greedy/greedy.py
+++ greedy/greedy.py
@@ -1,19 +1,25 @@
+SOPHIA = 0
+MATEO = 1
+
+MONEDA_IZQ = 0
+MONEDA_DER = -1
+
 def elegir_moneda(monedas, jugador):
     if not monedas:
         return 0
 
     # Determina la moneda a elegir: mayor para Sophia, menor para Mateo
-    if (jugador == "Sophia" and monedas[0] > monedas[-1]) or (jugador == "Mateo" and monedas[0] < monedas[-1]):
-        return monedas.pop(0)
+    if (jugador == SOPHIA and monedas[MONEDA_IZQ] > monedas[MONEDA_DER]) or (jugador == MATEO and monedas[MONEDA_IZQ] < monedas[MONEDA_DER]):
+        return monedas.pop(MONEDA_IZQ)
     else:
-        return monedas.pop(-1)
+        return monedas.pop(MONEDA_DER)
 
 def greedy(monedas):
     puntos_sophia = 0
     puntos_mateo = 0
     while monedas:
-        puntos_sophia += elegir_moneda(monedas, "Sophia")
-        puntos_mateo += elegir_moneda(monedas, "Mateo")
+        puntos_sophia += elegir_moneda(monedas, SOPHIA)
+        puntos_mateo += elegir_moneda(monedas, MATEO)
 
     return puntos_sophia, puntos_mateo
 
