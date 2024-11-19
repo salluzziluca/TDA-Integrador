11061335ade180c25398d034fed3a9675ea10ea5
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-11-19 13:42:23 -0300

    refactor dequeless

--- primera_parte/greedy.py
+++ primera_parte/greedy.py
@@ -1,28 +1,24 @@
-from collections import deque 
-
 SOPHIA = 0
 MATEO = 1
 
-MONEDA_IZQ = 0
-MONEDA_DER = -1
-
-def elegir_moneda(monedas, jugador):
-    if not monedas:
-        return 0
-
-    # Determina la moneda a elegir: mayor para Sophia, menor para Mateo
-    if (jugador == SOPHIA and monedas[MONEDA_IZQ] > monedas[MONEDA_DER]) or (jugador == MATEO and monedas[MONEDA_IZQ] < monedas[MONEDA_DER]):
-        return monedas.popleft() # Moneda izq
+def elegir_moneda(monedas, jugador, izq, der):
+    if (jugador == SOPHIA and monedas[izq] > monedas[der]) or (jugador == MATEO and monedas[izq] < monedas[der]):
+        return monedas[izq], izq + 1, der  
     else:
-        return monedas.pop() # Moneda der
+        return monedas[der], izq, der - 1  
 
 def greedy_monedas(monedas):
-    deque_monedes = deque(monedas) # dequeado paraque el pop(0) sea O(1)
-    puntos_sophia = 0
-    puntos_mateo = 0
-    while deque_monedes:
-        puntos_sophia += elegir_moneda(deque_monedes, SOPHIA)
-        puntos_mateo += elegir_moneda(deque_monedes, MATEO)
+    izq, der = 0, len(monedas) - 1  
+    puntos_sophia, puntos_mateo = 0, 0
+
+    turno_sophia = True
+    while izq <= der:
+        jugador = SOPHIA if turno_sophia else MATEO
+        moneda, izq, der = elegir_moneda(monedas, jugador, izq, der)
+        if turno_sophia:
+            puntos_sophia += moneda
+        else:
+            puntos_mateo += moneda
+        turno_sophia = not turno_sophia  
 
     return puntos_sophia, puntos_mateo
-
