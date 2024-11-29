fd8d3550c73d9a44bb6a260f44faae768aea13dd
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-11-29 20:29:02 -0300

    main para correr con argumentos

--- primera_parte/csv_casos.py
+++ primera_parte/csv_casos.py
@@ -1,7 +1,7 @@
 LINEA_TESTS = 1
 
 def csv_casos_a_lista(nombre_test):
-    with open(f'primera_parte/casos_test/{nombre_test}') as f:
+    with open(nombre_test) as f:
         linea = f.readlines()[LINEA_TESTS] # Picardías argentinas
 
         output = [int(s)  for s in linea[:-1].split(';')] 
--- primera_parte/tests.py
+++ primera_parte/tests.py
@@ -7,31 +7,31 @@ RESULTADO_MATEO = 1
 
 class TestGreedy(unittest.TestCase):
     def test_greedy_20(self):
-        result = greedy_monedas(csv_casos_a_lista("20.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/20.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
     
     def test_greedy_25(self):
-        result = greedy_monedas(csv_casos_a_lista("25.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/25.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
 
     def test_greedy_50(self):
-        result = greedy_monedas(csv_casos_a_lista("50.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/50.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
 
     def test_greedy_100(self):
-        result = greedy_monedas(csv_casos_a_lista("100.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/100.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
 
     def test_greedy_1000(self):
-        result = greedy_monedas(csv_casos_a_lista("1000.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/1000.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
     
     def test_greedy_10000(self):
-        result = greedy_monedas(csv_casos_a_lista("10000.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/10000.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
 
     def test_greedy_20000(self):
-        result = greedy_monedas(csv_casos_a_lista("20000.txt"))
+        result = greedy_monedas(csv_casos_a_lista("primera_parte/casos_test/20000.txt"))
         self.assertTrue(result[RESULTADO_SOPHIA]>result[RESULTADO_MATEO])
 
 
