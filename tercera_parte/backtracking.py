8651253e099e720e791b33b7dd12f12419a3dadc
AContiSG <84248592+AContiSG@users.noreply.github.com>
2024-12-02 16:59:43 -0300

    main BT + README

--- tercera_parte/backtracking.py
+++ tercera_parte/backtracking.py
@@ -180,36 +180,3 @@ def resolver_tablero(barcos, filas, columnas, demandas_filas, demandas_columnas)
         "demanda_total": demandas_filas + demandas_columnas,
         "posiciones_barcos": solucion[2]
     }
-
-def main():
-    # Cambiar el nombre del archivo según el caso
-    n,m,barcos, filas, columnas = procesar_archivo("tercera_parte/casos_test/12_12_21.txt")
-    demandas_filas = sum(filas)
-    demandas_columnas = sum(columnas)
-    
-    start = time.time()
-    solucion = resolver_tablero(barcos, filas, columnas, demandas_filas, demandas_columnas)
-    end = time.time()
-    
-    tablero = solucion["tablero_optimo"]
-    tablero_formateado = [[" - " if celda == -1 else f" {str(celda)} "  for celda in fila] for fila in tablero]
-    demanda_cumplida = solucion["demanda_cumplida"]
-    demanda_total = solucion["demanda_total"]
-    posiciones_barcos = solucion["posiciones_barcos"]
-
-    print("Tablero óptimo:")
-    for fila in tablero_formateado:
-        print(" ".join(map(str, fila)))
-    print("\nDemanda cumplida:", demanda_cumplida)
-    print("Demanda total:", demanda_total)
-    print("\nTiempo de ejecución:", end - start, "segundos")
-
-    # Imprimir las posiciones de los barcos
-    print("\nPosiciones de los barcos:")
-    for idx, pos in posiciones_barcos.items():
-        if pos is not None:
-            print(f"Barco {idx}: ({pos[0]},{pos[1]}) -> ({pos[2]},{pos[3]})")
-        else:
-            print(f"Barco {idx}: No colocado")
-
-#main()
