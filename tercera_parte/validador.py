235113587dede2364298805101ecc5dc2f77d435
gabrielPeraltaM <gperaltam@fi.uba.ar>
2024-12-02 23:20:52 -0300

    Cambios en el validador

--- tercera_parte/validador.py
+++ tercera_parte/validador.py
@@ -1,3 +1,4 @@
+# Verifica que el barco a ubicar no sea adyacente a otro ya ubicado
 def adyacente_horizontal(barco, tablero, fila, pos_ini, n, m):
     if pos_ini > 0 and (tablero[fila][pos_ini - 1] == 1 or (fila > 0 and tablero[fila - 1][pos_ini - 1] == 1) or
                         (fila < n - 1 and tablero[fila + 1][pos_ini - 1] == 1)):
@@ -7,7 +8,7 @@ def adyacente_horizontal(barco, tablero, fila, pos_ini, n, m):
                                 (fila < n - 1 and tablero[fila + 1][pos_ini + 1] == 1)):
         return False
 
-    for i in range(pos_ini, pos_ini + barco):
+    for i in range(pos_ini, pos_ini + barco):  # O(L) con L el largo del barco actual
         if (fila > 0 and tablero[fila - 1][i] == 1) or (fila < n - 1 and tablero[fila + 1][i] == 1):
             return False
 
@@ -42,15 +43,16 @@ def asignar_barco_vertical(barco, tablero, columna, pos_ini, acumulados_fila):
         acumulados_fila[i] += 1
 
 
+# Ubica un barco de forma horizontal en el tablero
 def ubicar_barco_horizontal(barco, tablero, demandas_filas, demandas_columnas, acum_fila, acum_columna):
     n = len(demandas_filas)
     m = len(demandas_columnas)
-    for i in range(n):
+    for i in range(n):  # O(m x n)
         if acum_fila[i] + barco > demandas_filas[i]:
             continue
 
         pos_primer_cero = -1
-        for j in range(m):
+        for j in range(m):  # O(m)
             if tablero[i][j] == 1 or (acum_columna[j] + 1 > demandas_columnas[j]):
                 pos_primer_cero = -1
                 continue
@@ -98,10 +100,17 @@ def validador_naval(demandas_filas, demandas_columnas, solucion):
     acumulados_fila = [0] * len(demandas_filas)
     acumulados_columna = [0] * len(demandas_columnas)
     tablero = [[0] * m for _ in range(n)]
-    for barco in solucion:
+    for barco in solucion:  # O(k)
+        if (barco[0] != barco[2] and barco[1] != barco[3]) or (barco[0] == barco[2] and barco[3] < barco[1] or
+                                                               barco[1] == barco[3] and barco[2] < barco[0]):
+            return False
+
+        # O(n x m)
         if barco[0] == barco[2] and not ubicar_barco_horizontal(barco[3] - barco[1] + 1, tablero, demandas_filas,
                                                                 demandas_columnas, acumulados_fila, acumulados_columna):
             return False
+
+        # O(n x m)
         if barco[1] == barco[3] and not ubicar_barco_vertical(barco[2] - barco[0] + 1, tablero, demandas_filas,
                                                               demandas_columnas, acumulados_fila, acumulados_columna):
             return False
